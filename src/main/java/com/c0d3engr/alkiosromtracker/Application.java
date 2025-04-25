b96f8054d29b8392e23668f0e0478db5b2985ca3
c0d3engr <c0d3engr@gmail.com>
2025-04-25 09:35:27 +0800

    sync

--- src/main/java/com/c0d3engr/alkiosromtracker/Application.java
+++ src/main/java/com/c0d3engr/alkiosromtracker/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlkiosromtrackerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AlkiosromtrackerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
