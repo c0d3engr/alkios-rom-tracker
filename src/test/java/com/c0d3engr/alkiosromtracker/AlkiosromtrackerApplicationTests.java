743374f571802f2e07ac17f7157c40e25d81d47f
c0d3engr <c0d3engr@gmail.com>
2025-04-25 09:30:54 +0800

    Updated Java dependency from JDK17 to JDK24 in pom.xml

---
+++ src/test/java/com/c0d3engr/alkiosromtracker/AlkiosromtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c0d3engr.alkiosromtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkiosromtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
