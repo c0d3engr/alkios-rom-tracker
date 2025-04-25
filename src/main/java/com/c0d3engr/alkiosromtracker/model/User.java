bc9e54a13c6b83a630d824e010dc246099255c45
c0d3engr <c0d3engr@gmail.com>
2025-04-25 19:39:01 +0800

    Added getters and setters to User.java class

--- src/main/java/com/c0d3engr/alkiosromtracker/model/User.java
+++ src/main/java/com/c0d3engr/alkiosromtracker/model/User.java
@@ -11,6 +11,35 @@ public class User {
     private String email;
     private String passwordHash;
 
-    // Getters and Setters
-    // ...
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 }
