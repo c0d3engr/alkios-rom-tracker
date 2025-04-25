484f062eccbcbe1aa7cf86dd403e7021e1661005
c0d3engr <c0d3engr@gmail.com>
2025-04-25 21:47:18 +0800

    Reversed test

--- src/main/java/com/c0d3engr/alkiosromtracker/controller/AccountController.java
+++ src/main/java/com/c0d3engr/alkiosromtracker/controller/AccountController.java
@@ -1,1 +1,36 @@
-test
+package com.c0d3engr.alkiosromtracker.controller;
+
+import com.c0d3engr.alkiosromtracker.model.Account;
+import com.c0d3engr.alkiosromtracker.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @PutMapping("/{id}")
+    public Account updateAccount(@PathVariable Long id, @RequestBody Account account) {
+        return accountService.updateAccount(id, account);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+    }
+}
